55896e75a9f682a1d0df6da47e8dd2add169528d
Glory42 <gorkemkaryol03@gmail.com>
2025-01-22 15:32:48 +0300

    made canvas drawing

--- script.js
+++ script.js
@@ -10,17 +10,33 @@ const barWidth = 10;
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
-function resetArray() {
-
+//initializing the array
+function generateArray() {
+    const numBars = Math.floor(canvasWidth / canvasHeight);
+    array = Array.from({length: numBars}, () => Math.floor(Math.random() * canvasHeight));
+    drawArray();
 }
 
+//drawing the array
+function drawArray() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    array.forEach((value, index) => {
+        ctx.fillstyle = "#4caf50";
+        ctx.fillRect(index * barWidth, canvasHeight - value, barWidth - 2, value);
+    });
+}
 
+generateArray();
+
+//bubble Sort Algorithm
 async function bubbleSort(){
     for(i = 0; i < array.length; i++){
         for(j = 0; j.array.length - 1; j++){
             if(array[j] > array[j + 1]){
+                // swapping part
                 [array[j], array[j + 1]] = [array[j + 1], array[j]]
-                
+                drawArray();
+                await sleep(50);
             }
         }
     }
