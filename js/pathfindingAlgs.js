210b9f9852d570b259eef4007a0defc0b9c50cac
Glory42 <gorkemkaryol03@gmail.com>
2025-01-23 17:33:42 +0300

    changed the font

--- js/pathfindingAlgs.js
+++ js/pathfindingAlgs.js
@@ -57,3 +57,57 @@ function handleCellClick(row, col){
 }
 
 createGrid();
+
+//Dijkstra’s Algorithm
+async function dijkstra() {
+    if(!startNode || !endNode) return alert('Set start and end nodes first!');
+
+    const unvisitedNodes = [];
+    startNode.distance = 0;
+    
+    grid.flat().forEach(node => unvisitedNodes.push(node));
+
+    while(unvisitedNodes.length > 0){
+        unvisitedNodes.sort((a, b) => a.distance - b.distance);
+
+        const currentNode = unvisitedNodes.shift();
+        if(currentNode.distance === Infinity) break;
+        if(currentNode.isWall) continue;
+        if(currentNode === endNode) break;
+        
+        updateNeighbors(currentNode);
+        document.querySelector(
+            `.cell[data-row="${currentNode.row}"][data-col="${currentNode.col}"]`
+        ).classList.add('visited');
+    }
+    
+    let currentNode = endNode;
+    while(currentNode.previousNode){
+        const {row, col} = currentNode.previousNode;
+        document.querySelector(`.cell[data-row="${row}"][data-col="${col}"]`).classList.add('path');
+        currentNode = currentNode.previousNode;
+    }
+}
+
+function updateNeighbors(node){
+    const neighbors = getNeighbors(node);
+    neighbors.array.forEach(neighbor => {
+        const newDistance = node.distance + 1;
+        if(newDistance < neighbor.distance){
+            neighbor.distance = newDistance;
+            neighbor.previousNode = node;
+        }
+    });
+}
+
+function getNeighbors(node) {
+    const { row, col } = node;
+    const neighbors = [];
+    if (row > 0) neighbors.push(grid[row - 1][col]);
+    if (row < rows - 1) neighbors.push(grid[row + 1][col]);
+    if (col > 0) neighbors.push(grid[row][col - 1]);
+    if (col < cols - 1) neighbors.push(grid[row][col + 1]);
+    return neighbors.filter(neighbor => !neighbor.isWall);
+}
+
+
